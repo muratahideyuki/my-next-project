dc7d0edf5ced29ab1e0d1d8f4e4d5def9d1e9d2b
muratahideyuki <chirichiri73@gmail.com>
2024-07-27 09:35:16 +0900

    5章まででで

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -1,63 +1,29 @@
 import Image from "next/image";
+import { getMembersList } from "@/app/_libs/microcms";
+import { MEMBERS_LIST_LIMIT } from "@/app/_constants";
 import styles from "./page.module.css";
-const data = {
-  contents: [
-    {
-      id: "1",
-      image: {
-        url: "/img-member1.jpg",
-        width: 240,
-        height: 240,
-      },
-      name: "デイビッド",
-      position: "ceo",
-      profile: "あああああああああああああああ",
-    },
-    {
-      id: "2",
-      image: {
-        url: "/img-member2.jpg",
-        width: 240,
-        height: 240,
-      },
-      name: "あああ",
-      position: "ceo",
-      profile: "あああああああああああああああ",
-    },
-    {
-      id: "3",
-      image: {
-        url: "/img-member3.jpg",
-        width: 240,
-        height: 240,
-      },
-      name: "いいい",
-      position: "ceo",
-      profile: "あああああああああああああああ",
-    },
-  ],
-};
 
-export default function Page() {
+export default async function Page() {
+  const data = await getMembersList({ limit: MEMBERS_LIST_LIMIT });
   return (
     <div className={styles.container}>
       {data.contents.length === 0 ? (
         <p className={styles.empty}>メンバーがとろうされてませ</p>
       ) : (
         <ul>
-          {data.contents.map((menber) => (
-            <li key={menber.id} className={styles.list}>
+          {data.contents.map((member) => (
+            <li key={member.id} className={styles.list}>
               <Image
-                src={menber.image.url}
+                src={member.image.url}
                 alt=""
-                width={menber.image.width}
-                height={menber.image.height}
+                width={member.image.width}
+                height={member.image.height}
                 className={styles.image}
               />
               <dl>
-                <dt className={styles.name}>{menber.name}</dt>
-                <dd className={styles.position}>{menber.position}</dd>
-                <dd className={styles.plofile}>{menber.plofile}</dd>
+                <dt className={styles.name}>{member.name}</dt>
+                <dd className={styles.position}>{member.position}</dd>
+                <dd className={styles.profile}>{member.profile}</dd>
               </dl>
             </li>
           ))}
